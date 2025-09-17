0e52947cc619838e5a7905cd2f895d2a1fd6b55b
SaffronSolid935 <l.sanfelice@web.de>
2025-09-17 20:08:53 +0200

    Added code documentation

--- FileManager.cs
+++ FileManager.cs
@@ -4,14 +4,28 @@ using SFile = System.IO.File;
 namespace OpenGameSync.SDK
 {
     /// <summary>
-    /// 
+    /// Represents a file, wich can be stored localy but also remotely.
     /// </summary>
     public class File
     {
+        //// <summary>
+        /// The relative file path (based on the local and remote root directories).
+        /// </summary>
         private string relativePath;
+
+        /// <summary>
+        /// The absolute local root directory where the file is stored.
+        /// </summary>
         private string localPath;
 
+        /// <summary>
+        /// The binary content of the file (loaded from either local or remote).
+        /// </summary>
         protected byte[] content;
+
+        /// <summary>
+        /// Gets the absolute path of the file in the local filesystem.
+        /// </summary>
         protected string LocalPath
         {
             get
@@ -20,20 +34,35 @@ namespace OpenGameSync.SDK
             }
         }
 
+        /// <summary>
+        /// Returns the Fileinfo, if the file exists in the local storage.
+        /// </summary>
         public FileInfo FileInfo
         {
             get
             {
+                if (!File.Exists(LocalPath))
+                {
+                    return null;
+                }
                 return new FileInfo(LocalPath);
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="File"/> class.
+        /// </summary>
+        /// <param name="localPath">The absolute path of the local root directory.</param>
+        /// <param name="relativeFilePath">The relative path of the file.</param>
         public File(string localPath, string relativeFilePath)
         {
             this.localPath = localPath;
             this.relativePath = relativeFilePath;
         }
 
+        /// <summary>
+        /// Clears the <see cref="content"/> variable.
+        /// </summary>
         public void Unload()
         {
             if (content != null)
@@ -44,6 +73,10 @@ namespace OpenGameSync.SDK
 
         }
 
+        /// <summary>
+        /// Loads the content from a local file.
+        /// </summary>
+        /// <returns></returns>
         public async Task LoadLocalAsync()
         {
             // Logger.Log("Read local file");
@@ -51,16 +84,30 @@ namespace OpenGameSync.SDK
             // Logger.Log($"Local file conent: {content}");
         }
 
+        /// <summary>
+        /// Loads the content from the remote file.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public virtual async Task LoadRemoteAsync()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Saves content in a local file.
+        /// </summary>
+        /// <returns></returns>
         public async Task SaveLocalAsync()
         {
             await SFile.WriteAllBytesAsync(LocalPath, content);
         }
 
+        /// <summary>
+        /// Saves content to a remote file.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public virtual async Task SaveRemoteAsync()
         {
             throw new NotImplementedException();
--- SyncManager.cs
+++ SyncManager.cs
@@ -1,8 +1,23 @@
 namespace OpenGameSync.SDK
 {
+    /// <summary>
+    /// Manages both remote and local saved files required for synchronization.<br/>
+    /// Because of custom file classes and remote syncing, it is recommended 
+    /// to inherit from <c>SyncManager</c> and provide a custom implementation.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of file being managed. Custom file classes can be used, 
+    /// but must inherit from <see cref="File"/>.</typeparam>
+
     public class SyncManager<T> where T : File
     {
+        /// <summary>
+        /// The absolute local root directory where the file is stored.
+        /// </summary>
         private string localPath;
+        /// <summary>
+        /// The absolute local root directory where the file is stored.
+        /// </summary>
         protected string LocalPath
         {
             get
@@ -10,23 +25,46 @@ namespace OpenGameSync.SDK
                 return localPath;
             }
         }
+        /// <summary>
+        /// A list of remote files. <br>
+        /// Usally updated with <see cref="FetchRemote"/>.
+        /// </summary>
         private List<T> remoteFiles = new List<T>();
+        /// <summary>
+        /// A list of local files. <br>
+        /// Usally updated with <see cref="FetchLocal"/> .
+        /// </summary>
         private List<T> localFiles = new List<T>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SyncManager"/> class. 
+        /// </summary>
+        /// <param name="localPath">The absolute path of the local root directory.</param>
         public SyncManager(string localPath)
         {
             this.localPath = localPath;
         }
 
+        /// <summary>
+        /// Fetches the remote storage. <br>
+        /// It updates the <see cref="remoteFiles"/> list.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public virtual async Task FetchRemote()
         {
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// Fetches the local storage (at <see cref="localPath"/>). <br>
+        /// It updates the <see cref="localFiles"/> list.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public void FetchLocal()
         {
             var filePaths = GetFiles(localPath);
             localFiles.Clear();
-            var loadFileTask = new List<Task>();
             foreach (var filePath in filePaths)
             {
                 var file = InitLocalFile(localPath, filePath.Substring(localPath.Length));
@@ -36,20 +74,47 @@ namespace OpenGameSync.SDK
             }
         }
 
-        protected virtual T InitLocalFile(string localPath, string absolutePath)
+        /// <summary>
+        /// Creates a new instance of <typeparamref name="T"/>. 
+        /// <typeparamref name="T"/> must be derived from <see cref="File"/>. 
+        /// If your custom file class defines a different constructor signature, override this method.
+        /// </summary>
+        /// <param name="localPath">The absolute path of the local root directory.</param>
+        /// <param name="absolutePath">The absolute path of the file.</param>
+        /// <returns>A new instance of <typeparamref name="T"/>.</returns>
+
+        protected virtual T InitLocalFile(string localPath, string relativePath)
         {
-            return (T)Activator.CreateInstance(typeof(T), localPath, absolutePath)!;
+            return (T)Activator.CreateInstance(typeof(T), localPath, relativePath)!;
         }
 
+        /// <summary>
+        /// Clears the <see cref="remoteFiles"/> list.
+        /// </summary>
         protected void ClearRemoteFiles()
         {
             remoteFiles.Clear();
         }
 
+
+        /// <summary>
+        /// Adds an file to the <see cref="remoteFiles"/> list.
+        /// </summary>
+        /// <param name="file"></param>
         protected void AddRemoteFile(T file)
         {
             remoteFiles.Add(file);
         }
+
+        /// <summary>
+        /// Synchronizes the local files to the remote storage.
+        /// </summary>
+        /// <remarks>
+        /// Before calling this method, you must call <c>FetchLocal</c> to populate the local file list. 
+        /// This method assumes that the file list has already been generated.
+        /// </remarks>
+        /// <returns>
+        /// </returns>
 
         public async Task SyncToRemote()
         {
@@ -62,7 +127,15 @@ namespace OpenGameSync.SDK
             await Task.WhenAll(tasks);
         }
 
-
+        /// <summary>
+        /// Synchronizes the remote files to the local storage.
+        /// </summary>
+        /// <remarks>
+        /// Before calling this method, you must call <c>FetchRemote</c> to populate the remote file list. 
+        /// This method assumes that the file list has already been generated.
+        /// </remarks>
+        /// <returns>
+        /// </returns>
         public async Task SyncToLocal()
         {
             var tasks = new List<Task>();
@@ -73,16 +146,30 @@ namespace OpenGameSync.SDK
             await Task.WhenAll(tasks);
         }
 
+        /// <summary>
+        /// Deletes the files in the remote storage. (Needs to be overridden)
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         protected virtual async Task ClearRemote()
         {
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// Uploads an file from local to remote.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         protected async Task UploadToRemote(T file)
         {
             await file.LoadLocalAsync();
             await file.SaveRemoteAsync();
         }
-
+        /// <summary>
+        /// Downloads an file from remote local.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         protected async Task DownloadToLocal(T file)
         {
             await file.LoadRemoteAsync();
@@ -90,7 +177,11 @@ namespace OpenGameSync.SDK
         }
 
 
-
+        /// <summary>
+        /// Lists all files in a directory on the local storage.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         private string[] GetFiles(string path)
         {
             List<string> files = new List<string>();
